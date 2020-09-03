474c5e09ba45fb01c1105b9f59faccb12a712f37
Yoel Hor <yoelhor@live.com>
2020-09-03 22:54:54 +0300

    Console app

---
+++ dotnet_core_symmetric_key/source-code/console-app/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdTokenBuilder
+{
+    class Program
+    {
+        static readonly string _signingKey = "VK62QTn0m1hMcn0DQ3RPYDAr6yIiSvYgdRwjZtU5QhI=";
+        static readonly string _issuer = $"https://localhost";
+        static readonly string _audience = "a489fc44-3cc0-4a78-92f6-e413cd853eae";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string JWT = BuildIdToken("My app name", " John Smith");
+            Console.WriteLine(JWT);
+        }
+
+        private static string BuildIdToken(string appName, string displayName)
+        {
+
+
+            // All parameters send to Azure AD B2C needs to be sent as claims
+            IList<System.Security.Claims.Claim> claims = new List<System.Security.Claims.Claim>();
+            claims.Add(new System.Security.Claims.Claim("appName", appName, System.Security.Claims.ClaimValueTypes.String, issuer));
+            claims.Add(new System.Security.Claims.Claim("displayName", displayName, System.Security.Claims.ClaimValueTypes.String, issuer));
+
+            // Note: This key phrase needs to be stored also in Azure B2C Keys for token validation
+            var securityKey = Encoding.UTF8.GetBytes(_signingKey);
+
+            var signingKey = new SymmetricSecurityKey(securityKey);
+            SigningCredentials signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+            // Create the token
+            JwtSecurityToken token = new JwtSecurityToken(
+                    _issuer,
+                    _audience,
+                    claims,
+                    DateTime.Now,
+                    DateTime.Now.AddDays(7),
+                    signingCredentials);
+
+            // Get the representation of the signed token
+            JwtSecurityTokenHandler jwtHandler = new JwtSecurityTokenHandler();
+
+            return jwtHandler.WriteToken(token);
+        }
+    }
+}
